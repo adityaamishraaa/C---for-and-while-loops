793c2aa421df6db65031768ca8cf10f0b39c1406
adityaamishraaa <118230920+adityaamishraaa@users.noreply.github.com>
2023-03-16 11:17:46 +0530

    Create code3.c

---
+++ code3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+int a=0;
+   for(int i=1;i<4;i++)
+   {
+       for(int j=1;j<=i;j++)
+       {
+       a++;
+           printf("%d",a);
+       }
+       printf("\n");
+   }
+
+    return 0;
+}
